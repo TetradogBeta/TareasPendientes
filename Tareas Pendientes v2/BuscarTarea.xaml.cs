4ae4ad6f47163d9220e0ab9e3c3b61bbe3e91c79
tetradog <tetradog@gmail.com>
2016-02-25 01:14:12 +0100

    guardado como son las funciones de BuscarTarea

--- Tareas Pendientes v2/BuscarTarea.xaml.cs
+++ Tareas Pendientes v2/BuscarTarea.xaml.cs
@@ -19,6 +19,7 @@ namespace Tareas_Pendientes_v2
     /// </summary>
     public partial class BuscarTarea : Window
     {
+        //filtra las tareas por: fecha,texto contenido,hecho o no hecho,herencia(muestra cmb con las posibilidades)
         public BuscarTarea()
         {
             InitializeComponent();
